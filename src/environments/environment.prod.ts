ee89d8adc89427a28078ce4df057c65c061b3cd0
GitHubArvind01 <kumararvind21390@gmail.com>
2026-04-20 01:10:02 +0530

    [ARVIND] : Updated project dependencies and environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'https://api.yourdomain.com', // Your real server
+  gatewayUrl: 'https://api.yourdomain.com',
   googleClientId: '880694243289-hmeu169t5g1etinlqr8td1cv9crfb0gq.apps.googleusercontent.com',
-  googleRedirectUri: 'https://yourdomain.com/auth'
+  googleRedirectUri: 'https://yourdomain.com/auth',
+  paypalClientId: 'YOUR_PAYPAL_CLIENT_ID_LIVE',
+  paypalEnv: 'production'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
-  production: true,
-  gatewayUrl: 'http://localhost:8080', // Your real server
+  production: false,
+  gatewayUrl: 'http://localhost:8080/api',
   googleClientId: '880694243289-hmeu169t5g1etinlqr8td1cv9crfb0gq.apps.googleusercontent.com',
-  googleRedirectUri: 'http://localhost:4200/auth'
+  googleRedirectUri: 'http://localhost:4200/auth',
+  paypalClientId: 'YOUR_PAYPAL_CLIENT_ID_SANDBOX',
+  paypalEnv: 'sandbox'
 };
