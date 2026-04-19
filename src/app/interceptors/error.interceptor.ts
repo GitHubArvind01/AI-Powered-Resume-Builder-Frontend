14a336767f51a5cd7d6b9c69debf78e6006ed795
GitHubArvind01 <kumararvind21390@gmail.com>
2026-04-20 00:13:02 +0530

    [ARVIND] : Added HTTP interceptors for auth/token handling

---
+++ src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      let userFriendlyMsg = '';
+
+      if (error.status === 0) {
+        userFriendlyMsg = 'The server is unreachable. Check your connection.';
+      } else if (error.error?.message) {
+        // Matches your Java ErrorResponse.message
+        userFriendlyMsg = error.error.message;
+      } else {
+        userFriendlyMsg = `Error: ${error.statusText}`;
+      }
+
+      // You can also log to a service like Sentry here
+      return throwError(() => new Error(userFriendlyMsg));
+    })
+  );
+};
