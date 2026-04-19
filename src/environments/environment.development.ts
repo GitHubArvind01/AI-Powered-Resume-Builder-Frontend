e3a48839900a84f70ee0623b850bdecd8928043d
GitHubArvind01 <kumararvind21390@gmail.com>
2026-04-20 00:13:54 +0530

    [ARVIND] : Added environment data for application

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  gatewayUrl: 'http://localhost:8080', // Your real server
+  googleClientId: '880694243289-hmeu169t5g1etinlqr8td1cv9crfb0gq.apps.googleusercontent.com',
+  googleRedirectUri: 'http://localhost:4200/auth'
+};
