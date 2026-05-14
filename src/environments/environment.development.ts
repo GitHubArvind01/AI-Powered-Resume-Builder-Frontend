c08bcba7998d0dc73480cc2750398d8372bfa9c2
Arvind Kumar <148184868+GitHubArvind01@users.noreply.github.com>
2026-05-14 15:19:24 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   gatewayUrl: 'GATEWAY_URL',
   googleClientId: 'GOOGLE_CLIENT_ID',
-  googleRedirectUri: 'GOOGLE_REDIRECT_URI'
+  googleRedirectUri: 'GOOGLE_REDIRECT_URI',
+  paypalClientId: 'YOUR_PAYPAL_CLIENT_ID_LIVE',
+  paypalEnv: 'production'
 };
