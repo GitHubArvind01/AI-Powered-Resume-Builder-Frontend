761c27d2ecd3fe63f7afed243af95c4b693dc73d
Arvind Kumar <148184868+GitHubArvind01@users.noreply.github.com>
2026-05-14 15:18:10 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'http://localhost:8080/api/v1', // Your real server
-  googleClientId: '880694243289-hmeu169t5g1etinlqr8td1cv9crfb0gq.apps.googleusercontent.com',
-  googleRedirectUri: 'http://localhost:4200/auth'
+  gatewayUrl: 'GATEWAY_URL',
+  googleClientId: 'GOOGLE_CLIENT_ID',
+  googleRedirectUri: 'GOOGLE_REDIRECT_URI'
 };
