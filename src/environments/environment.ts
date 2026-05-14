0906298aaa8ddcca599dbe6f69272e6f5af73423
Arvind Kumar <148184868+GitHubArvind01@users.noreply.github.com>
2026-05-14 15:20:06 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  gatewayUrl: 'http://localhost:8080/api/v1',
-  googleClientId: '880694243289-hmeu169t5g1etinlqr8td1cv9crfb0gq.apps.googleusercontent.com',
-  googleRedirectUri: 'http://localhost:4200/auth',
-  paypalClientId: 'YOUR_PAYPAL_CLIENT_ID_SANDBOX',
-  paypalEnv: 'sandbox'
+  gatewayUrl: 'GATEWAY_URL',
+  googleClientId: 'GOOGLE_CLIENT_ID',
+  googleRedirectUri: 'GOOGLE_REDIRECT_URI',
+  paypalClientId: 'YOUR_PAYPAL_CLIENT_ID_LIVE',
+  paypalEnv: 'production'
 };
